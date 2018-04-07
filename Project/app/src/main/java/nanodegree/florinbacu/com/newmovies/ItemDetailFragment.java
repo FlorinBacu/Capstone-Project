1a27530e49afe3f89533dd1e89c41b1e8063770f
Florin Bacu <bacuflorin@yahoo.com>
2018-04-07 14:49:17 +0300

    Add detail in android app

--- Project/app/src/main/java/nanodegree/florinbacu/com/newmovies/ItemDetailFragment.java
+++ Project/app/src/main/java/nanodegree/florinbacu/com/newmovies/ItemDetailFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,8 +46,8 @@ public class ItemDetailFragment extends Fragment {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
-            mItem = ContentLoader.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
+    mItem=ContentLoader.ITEMS.get(Integer.parseInt(getArguments().getString(ARG_ITEM_ID)));
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
@@ -62,7 +63,7 @@ public class ItemDetailFragment extends Fragment {
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+            ((TextView) rootView.findViewById(R.id.item_detail)).setText(Html.fromHtml(mItem.details));
         }
 
         return rootView;
